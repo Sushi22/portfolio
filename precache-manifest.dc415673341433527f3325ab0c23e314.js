7b5ff79c04d29a937ba1db9d550629c411ca1740
Sushi22 <sushmitajoshi22@gmail.com>
2020-09-09 14:55:33 +0530

    Updates

---
+++ precache-manifest.dc415673341433527f3325ab0c23e314.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa503a726d50184503a11b630286a97c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "39d41f2d10617a72eb70",
+    "url": "/portfolio/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "f3dfa76cb8d8d4d4011d",
+    "url": "/portfolio/static/css/main.26b6b7bb.chunk.css"
+  },
+  {
+    "revision": "39d41f2d10617a72eb70",
+    "url": "/portfolio/static/js/2.18393f1f.chunk.js"
+  },
+  {
+    "revision": "bdb08df6d197ae36b9622efbd218f2e4",
+    "url": "/portfolio/static/js/2.18393f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3dfa76cb8d8d4d4011d",
+    "url": "/portfolio/static/js/main.7e69437b.chunk.js"
+  },
+  {
+    "revision": "5585c7ca4551591612a7",
+    "url": "/portfolio/static/js/runtime-main.c60b095e.js"
+  },
+  {
+    "revision": "02150dafeb147876d0b71ae1dba105e9",
+    "url": "/portfolio/static/media/Zinc.02150daf.jpg"
+  },
+  {
+    "revision": "0aab739fe705a8d604c2778d766e8fac",
+    "url": "/portfolio/static/media/cousera.0aab739f.png"
+  },
+  {
+    "revision": "20c7fc9c84a9f0bcee381f21a96985ba",
+    "url": "/portfolio/static/media/crft4.20c7fc9c.png"
+  },
+  {
+    "revision": "b75240958c006ab1a23b8da7f4971305",
+    "url": "/portfolio/static/media/crop2.b7524095.png"
+  },
+  {
+    "revision": "f7ad32372eae813f1325efa33189c281",
+    "url": "/portfolio/static/media/deskfront2.f7ad3237.png"
+  },
+  {
+    "revision": "6d3c54839388907419c782e533d36881",
+    "url": "/portfolio/static/media/devc2.6d3c5483.png"
+  },
+  {
+    "revision": "7e832a2c7507d46500ffd0524e8ff1b1",
+    "url": "/portfolio/static/media/gssummit.7e832a2c.png"
+  },
+  {
+    "revision": "e28bffca2d5e6850fa91e758a000f4dd",
+    "url": "/portfolio/static/media/techstack.e28bffca.jpg"
+  }
+]);
--- src/components/context.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const flipContext = React.createContext({
-    flip: false,
-    setFlip: () => {}
-});
-
-export default flipContext;
